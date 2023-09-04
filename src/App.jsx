5af634255fc1a3e4de50672b66388f9d8541b277
dhiflaoui <dhiflaouiamira98@gmail.com>
2023-09-04 19:48:15 +0200

    add Home page

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Routes,Route } from 'react-router-dom'
 import CreateBooks from './pages/CreateBooks'
 import Home from './pages/Home'
 import DeleteBooks from './pages/DeleteBooks'
-import Edite from './pages/Edite'
+import EditeBooks from './pages/EditeBooks'
 import ShowBook from './pages/ShowBook'
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
        <Route path='/'element={<Home/>} />
        <Route path='/books/create'element={<CreateBooks/>} />
        <Route path='/books/delete'element={<DeleteBooks />} />
-       <Route path='/books/edite/:id'element={<EditeBook />}/>
+       <Route path='/books/edite/:id'element={<EditeBooks />}/>
        <Route path='/books/details/:id'element={<ShowBook/>} />
   
 </Routes>
---
+++ src/pages/EditeBooks.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EditeBooks = () => {
+  return (
+    <div>EditeBooks</div>
+  )
+}
+
+export default EditeBooks
